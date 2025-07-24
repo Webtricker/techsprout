3cadb284b7f7b1eaa0e89feec7b26a27d5b5a140
dev-ahad-ali <ahadali.dev@gmail.com>
2025-07-24 16:26:54 +0600

    Update: brands added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Title from '@/components/Title';
 import { Button } from '@/components/ui/button';
 import { TextBadge } from '@/components/ui/text-badge';
@@ -5,12 +6,15 @@ import { PhoneCall } from 'lucide-react';
 import Image from 'next/image';
 import bannerImg from '@/assets/img/banner_img.png';
 import AnimatedText from '@/components/AnimatedText';
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
+import { brands } from '@/lib/dummy-data';
+import Autoplay from 'embla-carousel-autoplay';
 
 export default function Home() {
   return (
     <main>
       {/* Banner Section */}
-      <div className="h-[780px] bg-[url('../assets/img/banner_bg.jpg')] bg-cover pt-40">
+      <div className="bg-[url('../assets/img/banner_bg.jpg')] bg-cover pt-40">
         <div className='container mx-auto grid grid-cols-1 items-center gap-8 px-4 md:grid-cols-2'>
           <div className='max-w-[570px] space-y-4'>
             <TextBadge>100% Satisfaction Guarantee</TextBadge>
@@ -40,6 +44,33 @@ export default function Home() {
           <div className='flex justify-center'>
             <Image src={bannerImg} alt='Banner Image' width={632} height={617} className='mt-8' />
           </div>
+        </div>
+      </div>
+      {/* Brands  */}
+      <div className='py-12'>
+        <div className='container mx-auto px-4'>
+          <Carousel
+            opts={{
+              align: 'center',
+              loop: true,
+            }}
+            plugins={[
+              Autoplay({
+                delay: 2000,
+              }),
+            ]}
+          >
+            <CarouselContent>
+              {brands.map((brand, idx) => (
+                <CarouselItem
+                  key={idx}
+                  className='flex basis-1/6 items-center justify-center opacity-50 grayscale-100 duration-300 select-none hover:opacity-100 hover:grayscale-0'
+                >
+                  <Image src={brand} width={120} height={50} alt='brand' />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
         </div>
       </div>
     </main>
---
+++ src/lib/dummy-data.ts
@@ -0,0 +1,9 @@
+import brand1 from '@/assets/img/brands/brand01.png';
+import brand2 from '@/assets/img/brands/brand02.png';
+import brand3 from '@/assets/img/brands/brand03.png';
+import brand4 from '@/assets/img/brands/brand04.png';
+import brand5 from '@/assets/img/brands/brand05.png';
+import brand6 from '@/assets/img/brands/brand06.png';
+import brand7 from '@/assets/img/brands/brand07.png';
+
+export const brands = [brand1, brand2, brand3, brand4, brand5, brand6, brand7];
