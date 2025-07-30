3a992244fd6405a5b5bf2dd7520976cbe88a943d
dev-ahad-ali <ahadali.dev@gmail.com>
2025-07-30 18:20:25 +0600

    Update: top instructor and cta section responsive done

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -5,18 +5,24 @@ import ctaBg from '@/assets/img/cta_bg.png';
 
 export default function Cta() {
   return (
-    <section className='bg-accent/20 custom-container relative grid grid-cols-4 items-center gap-4 rounded-xl bg-cover px-8 py-12'>
-      <Image src={ctaBg} alt='pattern' className='absolute inset-0 -z-10 grayscale-25' />
-      <h2 className='font-lexend col-span-1 text-[30px] leading-tight font-semibold text-white'>
-        Join Us & <AnimatedText text='Spread' /> Experience
-      </h2>
-      <p className='col-span-2 ms-2'>
-        Borem ipsum dolor sit amet, consectetur adipiscing eliawe awUt elit ellus, luctus nec
-        ullamcorper mattisBorem
-      </p>
-      <Button isArrow={true} size={'lg'} variant={'ghost'} className='col-span-1 bg-white'>
-        Become an Instructor
-      </Button>
+    <section className='custom-container'>
+      <div className='bg-accent/20 relative grid grid-cols-1 items-center gap-4 rounded-xl bg-cover px-8 py-12 lg:grid-cols-4'>
+        <Image
+          src={ctaBg}
+          alt='pattern'
+          className='absolute inset-0 -z-10 hidden grayscale-25 xl:block'
+        />
+        <h2 className='font-lexend col-span-1 text-[30px] leading-tight font-semibold text-white'>
+          Join Us & <AnimatedText text='Spread' /> Experience
+        </h2>
+        <p className='col-span-2 ms-2'>
+          Borem ipsum dolor sit amet, consectetur adipiscing eliawe awUt elit ellus, luctus nec
+          ullamcorper mattisBorem
+        </p>
+        <Button isArrow={true} size={'lg'} variant={'ghost'} className='col-span-1 bg-white'>
+          Become an Instructor
+        </Button>
+      </div>
     </section>
   );
 }
--- src/components/sections/home/TopInstructors.tsx
+++ src/components/sections/home/TopInstructors.tsx
@@ -9,7 +9,7 @@ export default function TopInstructors() {
   return (
     <section className='bg-white pt-[120px] pb-[180px]'>
       <div className='custom-container'>
-        <div className='flex items-center justify-between'>
+        <div className='flex flex-col items-center justify-between gap-8 md:flex-row'>
           <div className='space-y-4'>
             <TextBadge>Our Qualified People Matter</TextBadge>
             <Title h={2}>
@@ -22,7 +22,7 @@ export default function TopInstructors() {
             </Button>
           </div>
         </div>
-        <div className='mt-8 grid grid-cols-4 gap-8'>
+        <div className='mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4'>
           {instructors.map((instructor, idx) => (
             <InstructorCard key={instructor.id} instructor={instructor} order={idx + 1} />
           ))}
