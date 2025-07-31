4bf391bd8348e0b13ae82982a015f290e90ee584
dev-ahad-ali <ahadali.dev@gmail.com>
2025-07-31 18:27:10 +0600

    Update: user model created

---
+++ src/Models/User.model.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface User extends Document {
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  password: string;
+  occupation: string;
+  bio: string;
+  socialLinks: [string];
+  role: 'user' | 'admin' | 'instructor';
+}
+
+const userSchema: Schema<User> = new Schema(
+  {
+    name: String,
+    username: String,
+    email: { type: String, unique: true },
+    phone: String,
+    password: String,
+    occupation: String,
+    bio: String,
+    socialLinks: [String],
+    role: { type: String, enum: ['user', 'admin', 'instructor'], default: 'user' },
+  },
+  { timestamps: true }
+);
+
+export const User =
+  (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>('User', userSchema);
