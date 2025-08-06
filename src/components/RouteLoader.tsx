b7d009343ca01a4ada0fb419277222ab5d6a4ea8
dev-ahad-ali <ahadali.dev@gmail.com>
2025-08-06 21:35:32 +0600

    Update: full screen loader option if needed

--- src/components/RouteLoader.tsx
+++ src/components/RouteLoader.tsx
@@ -20,4 +20,11 @@ export default function RouteLoader() {
   if (!loading) return null;
 
   return <div className='bg-primary fixed top-0 left-0 z-50 h-1 w-full animate-pulse' />;
+
+  // if full screen needed
+  // return (
+  //   <div className='bg-opacity-90 fixed inset-0 z-[1000] flex items-center justify-center bg-white transition-opacity duration-300'>
+  //     <div className='border-primary h-12 w-12 animate-spin rounded-full border-4 border-t-transparent' />
+  //   </div>
+  // );
 }
