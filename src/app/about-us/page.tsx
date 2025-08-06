1f617e4c46cb7238260ab6864784f7f40f91f5c3
dev-ahad-ali <ahadali.dev@gmail.com>
2025-08-06 16:12:50 +0600

    Update: shapes added for who we are section in about us page

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -8,13 +8,16 @@ import Link from 'next/link';
 import aboutImg1 from '@/assets/img/about/about_img03.jpg';
 import aboutImg2 from '@/assets/img/about/about_img04.jpg';
 import aboutImg3 from '@/assets/img/about/about_img05.jpg';
+import aboutShape1 from '@/assets/img/shapes/about_shape02.png';
+import aboutShape2 from '@/assets/img/shapes/about_shape03.png';
+import aboutShape3 from '@/assets/img/shapes/about_dots.svg';
 
 export default function page() {
   return (
     <>
       <Hero pageName='About Us' />
       {/* Who we are */}
-      <section className='container mx-auto mt-18 grid grid-cols-3 items-center gap-4 px-4'>
+      <section className='container mx-auto mt-18 grid grid-cols-3 items-center gap-4 px-4 pb-24'>
         <div className='col-span-1 space-y-4'>
           <TextBadge>Who we are</TextBadge>
           <Title h={2}>
@@ -38,7 +41,7 @@ export default function page() {
             </Link>
           </div>
         </div>
-        <div className='col-span-2 flex items-center justify-center gap-4'>
+        <div className='relative col-span-2 flex items-center justify-center gap-4'>
           <Image src={aboutImg1} width={315} height={410} alt='about us' className='rounded-2xl' />
           <div className='flex flex-col gap-4'>
             <Image
@@ -56,6 +59,27 @@ export default function page() {
               className='rounded-2xl'
             />
           </div>
+          <Image
+            src={aboutShape1}
+            width={110}
+            height={105}
+            alt='shape'
+            className='absolute -top-6 right-12 z-[-1]'
+          />
+          <Image
+            src={aboutShape2}
+            width={120}
+            height={120}
+            alt='shape'
+            className='animate-spin-slow absolute right-16 -bottom-12 z-[-1]'
+          />
+          <Image
+            src={aboutShape3}
+            width={125}
+            height={110}
+            alt='shape'
+            className='absolute bottom-0 left-12 z-[-1]'
+          />
         </div>
       </section>
     </>
