2aaee569f078787f4ecd5bbfe3fd5c759883d2b1
dev-ahad-ali <ahadali.dev@gmail.com>
2025-08-09 21:23:52 +0600

    Update: otp zod schema added for input validation

--- src/components/OtpModal.tsx
+++ src/components/OtpModal.tsx
@@ -1,13 +1,25 @@
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 import {
   Dialog,
   DialogContent,
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from '@/components/ui/dialog';
 import { Dispatch, SetStateAction } from 'react';
-import { Button } from './ui/button';
+import { InputOTP, InputOTPGroup, InputOTPSlot } from './ui/input-otp';
+import { otpSchema } from '@/schemas/otpSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import z from 'zod';
+import { useForm } from 'react-hook-form';
 
 export default function OtpModal({
   open,
@@ -16,18 +28,52 @@ export default function OtpModal({
   open: boolean;
   setOpen: Dispatch<SetStateAction<boolean>>;
 }) {
+  const form = useForm<z.infer<typeof otpSchema>>({
+    resolver: zodResolver(otpSchema),
+    defaultValues: {
+      otp: '',
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof otpSchema>) {
+    console.log(data);
+  }
+
   return (
     <Dialog open={open}>
-      {/* <DialogTrigger>Open</DialogTrigger> */}
-      <DialogContent>
+      <DialogContent showCloseButton={false}>
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your account and remove your
-            data from our servers.
-          </DialogDescription>
+          <DialogTitle>Enter the verification code sent via email</DialogTitle>
         </DialogHeader>
-        <Button onClick={() => setOpen(false)}>Close</Button>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='w-2/3 space-y-6'>
+            <FormField
+              control={form.control}
+              name='otp'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Verification Code</FormLabel>
+                  <FormControl>
+                    <InputOTP maxLength={6} {...field}>
+                      <InputOTPGroup>
+                        <InputOTPSlot index={0} />
+                        <InputOTPSlot index={1} />
+                        <InputOTPSlot index={2} />
+                        <InputOTPSlot index={3} />
+                        <InputOTPSlot index={4} />
+                        <InputOTPSlot index={5} />
+                      </InputOTPGroup>
+                    </InputOTP>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type='submit' onClick={() => setOpen(false)}>
+              Submit
+            </Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
---
+++ src/schemas/otpSchema.ts
@@ -0,0 +1,7 @@
+import z from 'zod';
+
+export const otpSchema = z.object({
+  otp: z.string().min(6, {
+    message: 'Your OTP must be 6 characters.',
+  }),
+});
